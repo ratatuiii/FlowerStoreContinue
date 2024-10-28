8900dfc4a86aa4d9b1059bcca9def7f9d44d07e7
ratatuiii <orest.chupa@gmail.com>
2024-10-28 22:17:08 +0200

    Bug fix

--- src/main/java/com/example/demo/delivery/PostDelivery.java
+++ src/main/java/com/example/demo/delivery/PostDelivery.java
@@ -1,19 +1,4 @@
 package com.example.demo.delivery;
-
-// import Flower.Item;
-// import lombok.AllArgsConstructor;
-// import lombok.Getter;
-// import lombok.Setter;
-
-// import java.util.List;
-// @Getter @Setter @AllArgsConstructor
-// public class PostDelivery implements Delivery{
-//     private List<Item> items;
-//     @Override
-//     public String deliver() {
-//         return "Delivered with post successfully";
-//     }
-// }
 import com.example.demo.flowerStore.Item;
 import java.util.List;
 
